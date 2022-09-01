ec50be6472cbd9afb2bbb3f8721ba987abf96808
Beaasuncion <111472853+beaasuncion@users.noreply.github.com>
2022-09-01 09:59:57 +0800

    Add files via upload

---
+++ Gear.java
@@ -0,0 +1,12 @@
+public class Gear {
+    String name;
+    int defence;
+    Float weight;
+    String description;
+    String rarity;
+    Boolean hasElementalDamage;
+
+    public void ShowDescription () {;
+        System.out.println ("Description: " + description);
+    }
+}
---
+++ Weapon.java
@@ -0,0 +1,13 @@
+public class Weapon 
+{
+    int damage;
+    float weight;
+    String name;
+    String description;
+    String rarity;
+    boolean hasElementalDamage;
+
+    public void ShowDescription () {;
+    System.out.println ("Description: " + description);
+    }
+}
